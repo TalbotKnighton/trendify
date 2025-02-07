842867a98a45bb8df389803dec5d6a5896b934ed
Talbot Knighton <talbotknighton@gmail.com>
2025-02-06 17:42:43 -0800

    updates

--- src/grafana_api/__init__.py
+++ src/grafana_api/__init__.py
@@ -1,6 +1,7 @@
 """
-Note:
+Bug:
     The Grafana API is experimental and still in development.
+    Don't use it yet.
 """
 from grafana_api.transformations import *
 from grafana_api.dashboard import *
